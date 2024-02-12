0d898f8ea08a678848dd14116e4ffa3f0ac26723
codingmousey <daliniellee@gmail.com>
2024-02-12 03:55:05 +0100

    header + icon

--- Frontend/src/App.svelte
+++ Frontend/src/App.svelte
@@ -1,12 +1,15 @@
+<!-- Scripts -->
 <script>
 	export let name;
+	import Header from "./Header.svelte";
 </script>
 
+<!-- HTML -->
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Header/>
 </main>
 
+<!-- Css -->
 <style>
 	main {
 		text-align: center;
---
+++ Frontend/src/Header.svelte
@@ -0,0 +1,34 @@
+<!-- Scripts -->
+<script>
+</script>
+
+<!-- HTML -->
+<header>
+  <div class="title">
+    <span class="red_text">Junior</span> Job
+  </div>
+  <img src="/img/icon.png" alt="Zed Icon" class="icon" />
+ 
+</header>
+
+<!-- Css -->
+<style>
+
+  header {
+    background: #f7f5f5;
+  }
+
+  .title {
+    font-size: 26px;
+    font-weight: bold;
+  }
+
+  .icon {
+    max-width: 69px;
+  }
+
+  .red_text {
+    color: red;
+  }
+
+</style>
