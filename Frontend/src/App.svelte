c684d2d8639fb4057291679d1d8cdf83face48c5
codingmousey <daliniellee@gmail.com>
2024-02-12 04:17:14 +0100

    addded footer

--- Frontend/src/App.svelte
+++ Frontend/src/App.svelte
@@ -1,33 +1,31 @@
 <!-- Scripts -->
 <script>
-	export let name;
-	import Header from "./Header.svelte";
+  import Header from "./Header.svelte";
+  import Footer from "./Footer.svelte";
 </script>
 
 <!-- HTML -->
+<Header />
 <main>
-	<Header/>
+  <h2>Welcome to Junior Job main page!</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed erat
+    lectus. Cras aliquet ut dolor sed volutpat. Nullam elementum nulla quam.
+    Donec sapien eros, congue at urna vel, ornare condimentum mi. Donec aliquet,
+    tortor ac hendrerit rutrum, arcu leo consequat dolor, non viverra urna nulla
+    vel dolor. Nam at elit ligula. Donec velit risus, placerat ut justo ac,
+    sollicitudin fringilla lacus. Duis diam nisi, consectetur at sapien sed,
+    pharetra malesuada dui. Quisque ornare scelerisque scelerisque. Aliquam sit
+    amet interdum sapien, ut egestas nibh. Curabitur molestie rhoncus tempor.
+    Aliquam placerat eros vel elit tincidunt, nec feugiat mauris molestie.
+  </p>
 </main>
+<Footer />
 
 <!-- Css -->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    max-width: 80%;
+    margin: 30px auto;
+  }
 </style>
---
+++ Frontend/src/Footer.svelte
@@ -0,0 +1,23 @@
+<!-- Scripts -->
+<script>
+</script>
+
+<!-- HTML -->
+<footer>
+  <div class="div">&copy; 2024 Junior Job</div>
+</footer>
+
+<!-- Css -->
+<style>
+  footer {
+    color: grey;
+    text-align: center;
+    font-size: 16;
+    font-weight: bold;
+  }
+  
+  .div{
+    display: inline-block;
+    border-top: 1px solid grey;
+  }
+</style>
