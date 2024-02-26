60bf7950c247d7c1742ce0d0264567679870e192
codingmousey <daliniellee@gmail.com>
2024-02-26 22:01:52 +0100

    spring initializer files added for backend

---
+++ Backend/finalwork.backend/src/test/java/ali/trabi/finalwork/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.trabi.finalwork.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
