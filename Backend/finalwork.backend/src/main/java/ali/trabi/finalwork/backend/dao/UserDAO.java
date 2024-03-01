c14f7a175e99ec04f4f173caf5b683daf3bcf1e7
codingmousey <daliniellee@gmail.com>
2024-03-01 11:07:52 +0100

    login frontend met backend gekoppeld

--- Backend/finalwork.backend/src/main/java/ali/trabi/finalwork/backend/dao/UserDAO.java
+++ Backend/finalwork.backend/src/main/java/ali/trabi/finalwork/backend/dao/UserDAO.java
@@ -4,4 +4,5 @@ import ali.trabi.finalwork.backend.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserDAO extends CrudRepository<User, Integer> {
+    User getUserByUsername(String username);
 }
