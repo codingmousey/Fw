0902af7b002c9988104290f3076208600ebfe347
codingmousey <daliniellee@gmail.com>
2024-03-03 21:19:53 +0100

    creatign functionality for 'load preferences ' button

--- Frontend/src/Search.svelte
+++ Frontend/src/Search.svelte
@@ -4,6 +4,7 @@
   import { jobListings } from "./db.js";
   import { filteredJobListings } from "./db.js";
   import { derived } from "svelte/store";
+  import { prefs, fetchUserPrefs } from "./db.js";
   /* https://eternaldev.com/blog/introduction-to-svelte-derived-store */
   /* https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates */
 
@@ -43,6 +44,24 @@
     console.log("filtered jobs:", JSON.stringify(filteredJobs, null, 2));
     filteredJobListings.set(filteredJobs);
   }
+
+  async function handleLoadPreference() {
+    console.log("loading preferences and triggering search");
+    await fetchUserPrefs();
+
+    const filteredJobs = $jobListings.filter((i) => {
+      const filterLanguage =
+        !$prefs || i.programmingLanguages.some((tag) => $prefs.includes(tag));
+      const filterLocation =
+        !$prefs || i.city.toLowerCase().includes(location.toLowerCase());
+        console.log("filterLanguage: " + filterLanguage);
+        console.log("filterLocation: " + filterLocation);
+      return filterLanguage && filterLocation;
+    });
+
+    console.log("filtered jobs:", JSON.stringify(filteredJobs, null, 2));
+    filteredJobListings.set(filteredJobs);
+  }
 </script>
 
 <!-- HTML -->
@@ -51,7 +70,7 @@
     <h1>All junior jobs in one place</h1>
     <h3>
       Find your first job or look around where you can find your dream job! Load
-      up your <Button>preferences</Button> OR search:
+      up your <Button on:click={handleLoadPreference}>preferences</Button> OR search:
     </h3>
   </div>
 
