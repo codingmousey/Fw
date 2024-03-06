26058b3b7512a77a6ed3ac62efc3450c64880456
codingmousey <daliniellee@gmail.com>
2024-03-06 01:46:40 +0100

    removed hh;mm since most joblistings dont have an hour

--- Frontend/src/CardView.svelte
+++ Frontend/src/CardView.svelte
@@ -54,9 +54,6 @@
             day: "2-digit",
             month: "2-digit",
             year: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
           })}
         </p>
       </div>
